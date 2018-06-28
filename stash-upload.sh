652ec921c1539a30b750f67315b411b4b0152c94
Geraint <luffgd@gmail.com>
2018-06-28 08:41:43 +0100

    Update script

--- stash-upload.sh
+++ stash-upload.sh
@@ -1,23 +1,24 @@
 if [ "$#" -ne 4 ]
 then
-  echo "Usage: stash-upload.sh [url] [file] [username] [password]"
-  exit 1
+ 	echo "Usage: stash-upload.sh [url] [file] [username] [password]"
+ 	echo "	  (url = https://stash.reaper.fm/manage_file/...)"
+	exit 1
 fi
 
 # Magic: upload to Stash
 echo "Logging in to Stash"
-curl -v https://stash.reaper.fm/index.php -c cookies.txt \
+curl https://stash.reaper.fm/index.php -c cookies.txt \
 	--data "dologin=1" \
 	--data-urlencode "user=$3" \
 	--data-urlencode "password=$4" \
-	-L
+	-L | /dev/null
 
 echo "Uploading ZIP"
-curl -v $1 \
+curl $1 \
 	-b cookies.txt \
 	-F "replacement=@$2" \
 	-F "replace=1" \
 	-F "submit=Replace+File" \
-	-L
+	-L | grep -o "File updated!"
 
 rm cookies.txt
